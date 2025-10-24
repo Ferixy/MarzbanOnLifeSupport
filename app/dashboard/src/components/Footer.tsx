238f908767879c6d45046db312f68b78981f875c
Ferixy <77561374+Ferixy@users.noreply.github.com>
2025-10-24 15:28:10 +0330

    hotfix

--- app/dashboard/src/components/Footer.tsx
+++ app/dashboard/src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { BoxProps, HStack, Link, Text } from "@chakra-ui/react";
-import { ORGANIZATION_URL, REPO_URL } from "constants/Project";
+import {REPO_URL} from "constants/Project";
 import { useDashboard } from "contexts/DashboardContext";
 import { FC } from "react";
 
